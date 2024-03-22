97a7294de0468a140c79f05505fcdbbd15ff1344
Andrew <drew29799@gmail.com>
2024-03-22 03:11:39 +0300

    Add files

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Module containing method that calculates the fewest number of
+    operations needed to result in exactly n H characters in the file.
+"""
+
+
+def minOperations(n) -> int:
+    """Calculates the minimum number of operations needed
+        to result in exactly n H characters in the file"""
+    return 0
