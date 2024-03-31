45574981db85b4b5bb86f9be33a7d7ca963a5be1
Andrew <drew29799@gmail.com>
2024-03-31 21:11:50 +0300

    Refactor: Count lines after 10 matched lines

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,51 +6,6 @@ import re
 import signal
 
 
-def print_statistics(log_metadata: dict):
-    """Prints the log statistics of requests"""
-    print("File size: {}".format(log_metadata['total_file_size']))
-    for stat_code, count in sorted(log_metadata['status_code_count'].items()):
-        if count > 0:
-            print("{}: {}".format(stat_code, count))
-
-
-def signal_handler(sig, frame):
-    """Handles the CTRL+C command signal"""
-    print_statistics(log_metadata)
-    sys.exit(0)
-
-
-# Registering signal handler for CTRL+C
-
-# <IPAddress> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-LOG_PATTERN = re.compile(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) '
-                         r'- \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] '
-                         r'"GET /projects/(\d+) HTTP/1\.1" (\d{3}) (\d+)$')
-
-
-def read_input(log_metadata: dict):
-    """Read api request logs and update the log metadata"""
-    try:
-        for line in sys.stdin:
-            log_metadata['line_count'] += 1
-            # Filter out logs that match the valid pattern
-            matchedLog = re.match(LOG_PATTERN, line)
-
-            # Keep track of valid logs only
-            if matchedLog:
-                status_code = int(matchedLog.group(4))
-                log_metadata['total_file_size'] += int(matchedLog.group(5))
-                if status_code in log_metadata['status_code_count'].keys():
-                    log_metadata['status_code_count'][status_code] += 1
-
-            # Log statistics after every 10 requests
-            if log_metadata['line_count'] % 10 == 0:
-                print_statistics(log_metadata)
-    except Exception as error:
-        print_statistics(log_metadata)
-        raise error
-
-
 log_metadata: dict = {
     'line_count': 0,
     'total_file_size': 0,
@@ -65,5 +20,53 @@ log_metadata: dict = {
         500: 0
     }
 }
+
+
+def print_statistics(log_metadata: dict):
+    """Prints the log statistics of requests"""
+    print("File size: {}".format(log_metadata['total_file_size']))
+    for stat_code, count in sorted(log_metadata['status_code_count'].items()):
+        if count > 0:
+            print("{}: {}".format(stat_code, count))
+
+
+def signal_handler(sig, frame):
+    """Handles the CTRL+C command signal"""
+    print_statistics(log_metadata)
+    sys.exit(0)
+
+
+# <IPAddress> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+LOG_PATTERN = re.compile(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) '
+                         r'- \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{6})\] '
+                         r'"GET /projects/(\d+) HTTP/1\.1" (\d{3}) (\d+)$')
+
+
+def read_input(log_metadata: dict):
+    """Read api request logs and update the log metadata"""
+    try:
+        for line in sys.stdin:
+            # Filter out logs that match the valid pattern
+            matchedLog = re.match(LOG_PATTERN, line)
+
+            # Keep track of valid logs only
+            if matchedLog:
+                log_metadata['line_count'] += 1
+                status_code = int(matchedLog.group(4))
+                log_metadata['total_file_size'] += int(matchedLog.group(5))
+                if status_code in log_metadata['status_code_count'].keys():
+                    log_metadata['status_code_count'][status_code] += 1
+
+            # Log statistics after every 10 requests
+            if log_metadata['line_count'] % 10 == 0:
+                print_statistics(log_metadata)
+    except Exception as error:
+        print_statistics(log_metadata)
+        raise error
+
+
+# Registering signal handler for CTRL+C
 signal.signal(signal.SIGINT, signal_handler)
+
+# Read logs piped inp and parse it
 read_input(log_metadata)
