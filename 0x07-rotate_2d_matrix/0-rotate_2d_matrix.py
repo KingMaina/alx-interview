c65c2feeef1e67c9bf8ac4b2a5ca13c71f02a997
Andrew <drew29799@gmail.com>
2024-04-25 23:57:45 +0300

    Add: Rotate 2-D matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Rotate a 2D Matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates a 2 dimension matrix
+
+        The matrix is edited inplace
+    """
+    return matrix
